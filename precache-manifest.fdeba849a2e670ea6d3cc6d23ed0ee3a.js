5dd75b60b97d6cce86976ac05172a027e700e592
mohitkumartoshniwal <mohitkrtoshniwal@gmail.com>
2021-04-02 14:37:41 +0530

    deploy

---
+++ precache-manifest.fdeba849a2e670ea6d3cc6d23ed0ee3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa104876faada391a2e8",
+    "url": "css/app.8caa578c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "076d6d06dc078d40139360653fa4b40a",
+    "url": "index.html"
+  },
+  {
+    "revision": "aa104876faada391a2e8",
+    "url": "js/app.c2ac66ed.js"
+  },
+  {
+    "revision": "7c8d40a4ff98b8303152",
+    "url": "js/chunk-vendors.db0dc09a.js"
+  },
+  {
+    "revision": "10cfab1651dc97532e5b68ec305d1093",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
